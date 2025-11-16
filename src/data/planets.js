136be4b0afae146a9af2d7429a1be7fc0381a44c
Yusuf5445 <Ylatipov007@gmail.com>
2025-11-16 15:40:21 +0500

    Js qismi boldi

---
+++ src/data/planets.js
@@ -0,0 +1,42 @@
+export const planets = [
+  {
+    id: "Mercury",
+    name: "Mercury",
+    Image: "/assets/image/merkury.webp",
+    description: "Mercury is the smallest planet and closest to the Sun",
+    radius: "2,439 km",
+    orbitalPeriod: "88 days",
+  },
+  {
+    id: "Venera",
+    name: "Venera",
+    Image: "/assets/image/venera.jpg",
+    description: "Venus is the second planet with a toxic atmosphere.",
+    radius: "6,051 km",
+    orbitalPeriod: "225days"
+  },
+  {
+    id: "Yer",
+    name: "Yer",
+    Image: "/assets/image/yer.jpg",
+    description: "Earth is the only known planet with life.",
+    radius: "6,371 km",
+    orbitalPeriod: "365 days",
+  },
+  {
+    id: "Mars",
+    name: "Mars",
+    Image: "/assets/image/mars.jpg",
+    description: "Mars is known as the Red Planet.",
+    radius: "3,389 km",
+    orbitalPeriod: "687 days",
+  },
+  {
+    id: "Saturin",
+    name: "Saturin",
+    Image: "/assets/image/saturin.png",
+    description: "Saturin is the only know planet",
+    radius: "60,268 km.",
+    orbitalPeriod: " 29.5 year",
+  }
+]
